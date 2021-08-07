a3115bec72040a6784834c4af1920a2c414940d7
samuelrojasp <samuel.rojasp@gmail.com>
2021-08-06 23:36:17 -0400

    creates component for quantities; fixes on routes

---
+++ src/components/Quantities.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="quantities">
+    <SavingLabel></SavingLabel>
+    <input type="number" v-model="quantity" class="input-quantity" />
+  </div>
+</template>
+<script>
+import SavingLabel from "@/components/SavingLabel";
+export default {
+  data: function () {
+    return {
+      quantity: 0,
+    };
+  },
+  components: {
+    SavingLabel,
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Order from "../views/Order.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/order",
+    name: "Order",
+    component: Order,
   },
 ];
 
