b826118ee18e704c5e79e2957f86ab3b2ee67eed
samuelrojasp <samuel.rojasp@gmail.com>
2021-08-06 23:50:35 -0400

    feat: creates a saving state message component

---
+++ src/components/SavingLabel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      isSaving: false,
+      isIdle: true,
+    };
+  },
+  computed: {
+    message: function () {
+      if (this.isSaving) {
+        return "Auto saving...";
+      }
+
+      if (!this.isSaving && !this.isIdle) {
+        return "Saved!";
+      }
+
+      return "";
+    },
+  },
+};
+</script>
