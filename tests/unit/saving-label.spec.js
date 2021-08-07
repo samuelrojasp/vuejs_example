87c81225f4322e784cdc158c8a5e0c8daef2a495
samuelrojasp <samuel.rojasp@gmail.com>
2021-08-06 23:49:32 -0400

    defines test cases for saving state messages component

---
+++ tests/unit/saving-label.spec.js
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import SavingLabel from "@/components/SavingLabel.vue";
+
+let wrapper = null;
+
+beforeEach(() => {
+  wrapper = shallowMount(SavingLabel);
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+describe("Messages shown according to states", () => {
+  it("shows auto saving message when is in saving state", async () => {
+    const message = wrapper.find("p");
+    await wrapper.setData({ isSaving: true });
+
+    expect(message.text()).to.contain("Auto saving...");
+  });
+
+  it("shows saved message when the data was saved", async () => {
+    const message = wrapper.find("p");
+    await wrapper.setData({ isSaving: false, isIdle: false });
+
+    expect(message.text()).to.contain("Saved!");
+  });
+
+  it("hides any messages when the state is idle", async () => {
+    const message = wrapper.find("p");
+    await wrapper.setData({ isSaving: false, isIdle: true });
+
+    expect(message.text()).to.equal("");
+  });
+});
