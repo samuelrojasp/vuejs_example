5a78ff878ad0bf34f72a6047af0245b5e04bc70e
samuelrojasp <samuel.rojasp@gmail.com>
2021-08-07 00:37:29 -0400

    saves after 2s of inactivity; shows saved after 1s; hides the saved message 2s after

--- src/components/Quantities.vue
+++ src/components/Quantities.vue
@@ -4,7 +4,12 @@
       v-bind:is-saving="isSaving"
       v-bind:is-idle="isIdle"
     ></SavingLabel>
-    <input type="number" v-model="quantity" class="input-quantity" />
+    <input
+      type="number"
+      v-model="quantity"
+      class="input-quantity"
+      v-on:keyup="resetTimer"
+    />
   </div>
 </template>
 <script>
@@ -15,10 +20,49 @@ export default {
       quantity: 0,
       isSaving: false,
       isIdle: true,
+      idleTime: 0,
+      idleTimer: null,
+      keyStrokes: 0,
     };
   },
   components: {
     SavingLabel,
+  },
+  mounted: function () {
+    this.setTimer();
+  },
+  methods: {
+    setTimer: function () {
+      this.idleTimer = setInterval(this.checkInterval, 1000);
+    },
+    resetTimer: function () {
+      this.keyStrokes++;
+      clearInterval(this.idleTimer);
+      this.idleTime = 0;
+      this.setTimer();
+    },
+    checkInterval: function () {
+      if (this.idleTime === 2 && this.keyStrokes > 0) {
+        this.autoSave();
+      }
+
+      if (this.idleTime === 5) {
+        this.hideMessage();
+      }
+
+      this.idleTime++;
+    },
+    autoSave: function () {
+      this.isSaving = true;
+
+      setTimeout(() => {
+        this.isSaving = false;
+        this.isIdle = false;
+      }, 1000);
+    },
+    hideMessage: function () {
+      this.isIdle = true;
+    },
   },
 };
 </script>
